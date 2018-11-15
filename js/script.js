1baf4ba4e6568bdd566514edfe530078a36eb27b
maggie <maggie.papadimas@hotmail.com>
2018-11-14 20:19:05 -0500

    added p5

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Don't Text And Drive
+// by Magdalene Papadimas
+//
+// this prototype will be a single player top down view side scroller.
+//
+//
+
+function setup() {
+  createCanvas(500,500);
+  background(0);
+}
+
+function draw () {
+  fill(250);
+  textSize(50);
+  text("hi", 250, 250);
+}
